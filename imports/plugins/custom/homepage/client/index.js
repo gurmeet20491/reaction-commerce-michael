ce7fbc55846f8c311387c0c9f94371154830267f
Raymond <raymondfok39@hotmail.com>
2017-11-25 02:57:57 +0830

    header

--- imports/plugins/custom/homepage/client/index.js
+++ imports/plugins/custom/homepage/client/index.js
@@ -1,5 +1,8 @@
+import './components/brand.js';
+import './containers/navbar.js';
 import "./static";
 import "./templates";
 import "./defaults";
 import "./styles/layout.less";
 import "./styles/footer.less";
+import "./styles/header.less";
